856e1797cdf448b35822b4fb114835831b3f3f14
rplra <lyaraazhar@gmail.com>
2025-02-11 08:09:40 +0800

    Chore: Uploaded builtins & checklist

--- builtin/builtin_export.c
+++ builtin/builtin_export.c
@@ -6,7 +6,7 @@
 /*   By: rraja-az <rraja-az@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 13:06:57 by rraja-az          #+#    #+#             */
-/*   Updated: 2025/02/06 13:07:17 by rraja-az         ###   ########.fr       */
+/*   Updated: 2025/02/10 17:28:02 by rraja-az         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,50 @@
 
 /*
 	EXPORT
-	DESC: Prints working directory (absolute path; /home/user/projects)
+	DESC: Create / modify env and make them available to child process
 	
+	EG	: (1) export a NEW var > export VAR=value (make var/val part of env and shell)
+		: (2) modify EXISTING var > export VAR=new_value (updates var with new value)
+		: (3) LIST exported vars > export > prints all exported vars alphabetically
+		
+	FLOW : 
+		1. Check args
+			- no args > print list of exported vars ALPHABETICALLY (bubble sort)
+			- has args > process each one
+		2. Validate args
+			- valid identifier (NAME=value format, start with letter/underscore)
+			- reject invalid names
+		3. Update / Add to env
+			- if env exist > update its value
+			- if it doesnt exist > add to array
+		4. Mark the var as exported
+			- make it available for child process
+		5. Handle memory
+			- malloc for new vars
 */
+
+static void	sort_export_var(char *var, t_shell *shell)
+{
+	
+}
+
+static void	print_export_var(char *var, t_shell *shell)
+{
+	
+}
+
+int	builtin_export(char **args, t_shell *shell)
+{
+	int	i;
+
+	i = 1;
+	if (!args[i])
+		print_export_var(shell->env[i], shell);
+	while (args[i])
+	{
+		if (is_env_name(args[i], shell->env[i]))
+			update_env(args[i], get_value(args[i]), false, shell);
+		else
+			update_env(args[i], get_value(args[i]), true, shell);
+	}
+}
---
+++ builtin/builtin_unset.c
@@ -0,0 +1,98 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtin_unset.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rraja-az <rraja-az@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/10 09:56:27 by rraja-az          #+#    #+#             */
+/*   Updated: 2025/02/10 15:14:57 by rraja-az         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "builtin.h"
+#include "../include/minishell.h"
+
+/*
+	DESC: Remove env / shell vars
+		: If var was exported, it no longer be available to child process
+	  EG: run unset MY_VAR, MY_VAR is removed from env
+		: if MY_VAR was exported, it no longer is available to any subprocess
+			spawned after unset
+	
+	FLOW:
+		1. Check for no args > print error > return error 
+		2. Check args > check for invalid > print error 
+			> else > var valid > remove var
+		3. return status
+*/
+
+static bool	is_env_var(char *var, t_shell *shell)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (shell->env[i])
+	{
+		j = 0;
+		while (var[j] && shell->env[i][j] && shell->env[i][j] != '=')
+		{
+			if (var[j] != shell->env[i][j])
+				break ;
+			j++;
+		}
+		if (var[j] == '\0' && shell->env[i][j] == '=')
+			return (true);
+		i++;
+	}
+	return (false);
+}
+
+static void	remove_env_var(char *var, t_shell *shell)
+{
+	int	i;
+	
+	i = 0;
+	// find index of var
+	while (shell->env[i])
+	{
+		if ((ft_strncmp(shell->env[i], var, ft_strlen(var)) == 0) && (shell->env[i][ft_strlen(var)] == '='))
+			break;
+		i++;
+	}
+	// if var not found
+	if (!shell->env[i])
+		return;
+	// free(shell->env[i]); // is it malloced?
+	// shift elements left
+	while (shell->env[i + 1])
+	{
+		shell->env[i] = shell->env[i + 1];
+		i++;
+	}
+	shell->env[i] = NULL; 
+}
+
+int	builtin_unset(char **args, t_shell *shell)
+{
+	int	i;
+
+	i = 1;
+	if (!args[i])
+	{
+		printf("unset: not enough arguments\n");
+		shell->last_exit_status = 1;
+		return (shell->last_exit_status);
+	}
+	while (args[i])
+	{
+		if (!is_env_var(args[i], shell))
+			printf("unset: variable does not exist\n");
+		else
+			remove_env_var(args[i], shell);
+		i++;
+	}
+	shell->last_exit_status = 0;
+	return (shell->last_exit_status);
+}
