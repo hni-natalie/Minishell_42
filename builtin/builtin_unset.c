77d482804d6c8e90744bf9b5c1cbb926af3425e6
Raja Puteri Lyara Raja Azhar <rraja-az@u90z03s01.42kl.edu.my>
2025-02-24 14:56:48 +0800

    Removed embedded Git repository

--- builtin/builtin_unset.c
+++ builtin/builtin_unset.c
@@ -6,7 +6,7 @@
 /*   By: rraja-az <rraja-az@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 09:56:27 by rraja-az          #+#    #+#             */
-/*   Updated: 2025/02/24 08:17:30 by rraja-az         ###   ########.fr       */
+/*   Updated: 2025/02/24 13:52:17 by rraja-az         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -101,8 +101,8 @@ int	builtin_unset(char **argv, t_shell *shell)
 	i = 1;
 	if (!argv[i])
 	{
-		ft_putstr_fd("unset: not enough arguments\n", 2);
-		shell->last_exit_status = FAILURE;
+		//ft_putstr_fd("unset: not enough arguments\n", 2);
+		shell->last_exit_status = SUCCESS;
 		return (shell->last_exit_status);
 	}
 	while (argv[i])
