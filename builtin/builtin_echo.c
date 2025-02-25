10ef149b22a9284e6648cc6611782b282063e6cd
Ho Ni Xuan <hni-xuan@u91z05s01.42kl.edu.my>
2025-02-25 13:35:55 +0800

    update echo header

--- builtin/builtin_echo.c
+++ builtin/builtin_echo.c
@@ -6,11 +6,11 @@
 /*   By: hni-xuan <hni-xuan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 14:20:27 by rraja-az          #+#    #+#             */
-/*   Updated: 2025/02/24 15:06:22 by hni-xuan         ###   ########.fr       */
+/*   Updated: 2025/02/25 13:32:48 by hni-xuan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
+#include "minishell.h"
 /* 
 	ECHO
 	DESC: prints arg to stdout, seperated by sapce
@@ -70,42 +70,3 @@ int	builtin_echo(char **argv, t_shell *shell)
 	shell->last_exit_status = SUCCESS;
 	return (shell->last_exit_status);
 }
-
-void	print_arguments(char **argv, int *i, t_shell *shell)
-{
-	int	j;
-
-	while (argv[*i])
-	{
-		j = -1;
-		// printf("argv_with_qoutes: %d\n", shell->argv_with_qoutes); // debug
-		while (argv[*i][++j])
-		{
-			if (argv[*i][j] == '\\' && shell->argv_with_qoutes == 0)
-				continue ;
-			write(1, &argv[*i][j], 1);
-		}
-		if (argv[(*i) + 1])
-			write(1, " ", 1);
-		(*i)++;
-	}
-}
-
-int	builtin_echo(char **argv, t_shell *shell)
-{
-	int		i;
-	bool	suppress_n;
-	
-	i = 0;
-	suppress_n = false;
-	while (argv[++i] && argv[i][0] == '-' && argv[i][1] == 'n' 
-		&& ft_strspn(argv[i] + 1, "n") == ft_strlen(argv[i]) - 1)
-		suppress_n = true;
-	print_arguments(argv, &i, shell);
-	if (!argv[i] && !suppress_n)
-		write (1, "\n", 1);
-	shell->last_exit_status = SUCCESS;
-	return (shell->last_exit_status);
-}
-
-
