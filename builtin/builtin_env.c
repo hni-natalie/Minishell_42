edbb7bae1b69e4652fa8b5a4c24f031cd8df62ac
rplra <lyaraazhar@gmail.com>
2025-03-07 23:43:33 +0800

    Fix: unset PATH works as BASH

--- builtin/builtin_env.c
+++ builtin/builtin_env.c
@@ -6,7 +6,7 @@
 /*   By: rraja-az <rraja-az@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 13:22:08 by rraja-az          #+#    #+#             */
-/*   Updated: 2025/02/28 10:24:39 by rraja-az         ###   ########.fr       */
+/*   Updated: 2025/03/07 23:36:45 by rraja-az         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,22 @@
 	ENV (with no options or arguments)
 	DESC: Prints all environment variables in newline
 */
+
 int	builtin_env(char **args, t_shell *shell)
 {
-	int	i;
+	int		i;
+	char	*path;
 
 	if (args[1])
 	{
 		shell->last_exit_status = FAILURE;
+		return (shell->last_exit_status);
+	}
+	path = get_directory("PATH", shell);
+	if (!path || !path[0])
+	{
+		ft_putstr_fd("minishell: env: No such file or directory\n", 2);
+		shell->last_exit_status = 127;
 		return (shell->last_exit_status);
 	}
 	i = 0;
